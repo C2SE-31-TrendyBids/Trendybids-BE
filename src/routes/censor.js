1704832ca6b308b2fcf85d7215f249b63539c8e9
Thien Quang <toiquang24@gmail.com>
2024-03-14 19:53:57 +0700

    Edit Censor routes

--- src/routes/censor.js
+++ src/routes/censor.js
@@ -7,16 +7,16 @@ const { verifyToken, isAdmin, isCensor } = require("../middlewares/verifyToken")
 
 
 const upload = multer({ storage: multer.memoryStorage() });
-router.post("/register-censor", upload.single('avatar'), censorController.registerCensor);
+router.post("/register-censor", upload.single('avatar'), censorControllers.registerCensor);
 router.get("/get-auction-session", censorControllers.getAuctionByQuery);
 router.get("/get-censor", censorControllers.getCensorByQuery);
 
 router.use(verifyToken)
 router.use(isCensor)
-router.post('/approve-auction-product', censorController.approveAuctionProduct)
-router.post('/post-auction-session', censorController.postAuctionSession)
-router.put('/update-auction-session/:sessionId', censorController.updateAuctionSession)
-router.delete('/delete-auction-session/:sessionId', censorController.deleteAuctionSession)
+router.post('/approve-auction-product', censorControllers.approveAuctionProduct)
+router.post('/post-auction-session', censorControllers.postAuctionSession)
+router.put('/update-auction-session/:sessionId', censorControllers.updateAuctionSession)
+router.delete('/delete-auction-session/:sessionId', censorControllers.deleteAuctionSession)
 
 
 module.exports = router
